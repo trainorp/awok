c3d3c29cef5cd1063af11bc1719926f2c69fa021
trainorp <patrick.trainor@louisville.edu>
2017-11-20 19:23:12 -0500

    Commit this bitch

--- comparison.R
+++ comparison.R
@@ -1,16 +1,23 @@
 ############ Prereqs ############
-ptm<-proc.time()
+# ptm<-proc.time()
 options(stringsAsFactors=FALSE)
 library(randomForest)
 library(e1071)
 library(nnet)
+library(glmnet)
 library(cvTools)
 library(dplyr)
+library(tidyr)
 library(doParallel)
+library(ggplot2)
+library(gridExtra)
 
-# oldPar<-par()
+oldPar<-par()
 
-# setwd("~/gdrive/AthroMetab/Data")
+setwd("~/gdrive/AthroMetab/WoAC")
+
+## Do not run:
+setwd("~/gdrive/AthroMetab/Data")
 spec<-read.csv("AthroACSRawSpectra.csv")
 key<-read.csv("metabolite_key2.csv")
 key[grepl("X - ",key$biochemical),]$biochemical<-key[grepl("X - ",key$biochemical),]$Unknown.Name
@@ -48,11 +55,11 @@ pheno$ptid2[pheno$group=="Type 2 MI"]<-sample(1:length(pheno$ptid2[pheno$group==
 metab<-cbind(group=pheno$group,metab)
 metab$group<-factor(make.names(metab$group))
 
-# setwd("~/gdrive/AthroMetab/WoAC")
+setwd("~/gdrive/AthroMetab/WoAC")
 
 ########### Random Forest importance ###########
-cl<-makeCluster(10)
-registerDoParallel(cl)
+# cl<-makeCluster(10)
+# registerDoParallel(cl)
 
 metab1<-metab
 bfe<-data.frame(iter=1:(ncol(metab1)-1))
@@ -93,7 +100,88 @@ for(i in 1:nrow(bfe)){
   print(i)
 }
 
-print(proc.time()-ptm)
-stopCluster(cl)
+# print(proc.time()-ptm)
+# stopCluster(cl)
 
 save.image(file="compare.RData")
+
+## End do not run:
+load(file="compare.RData")
+
+# RF plot:
+plot(bfe$iter,bfe$err,type="l")
+
+p1<-ggplot(bfe %>% filter(is.finite(err)),aes(x=iter,y=err))+geom_line()+theme_bw()+
+  xlab("Features (metabolites) eliminated")+ylab("Cross-entropy error (Repeated 10-fold CV)")
+
+########### Lasso / Elastic net ###########
+# List of elastic nets:
+tGLMlist<-list()
+for(alpha in seq(.1,1,.1)){
+  tGLM<-glmnet(x=as.matrix(metab[,names(metab)!="group"]),alpha=alpha,
+               y=metab[,"group"],family="multinomial",lambda=10**(-seq(0.44,6,.02)))
+  tGLMlist[[as.character(alpha)]]<-tGLM
+}
+# CV version:
+tGLMcvlist<-list()
+for(alpha in seq(.1,1,.1)){
+  tGLMcv<-cv.glmnet(x=as.matrix(metab[,names(metab)!="group"]),alpha=alpha,
+               y=metab[,"group"],family="multinomial",lambda=10**(-seq(0.44,6,.02)),
+               type.measure="deviance")
+  tGLMcvlist[[as.character(alpha)]]<-tGLMcv
+}
+
+tGLM<-tGLMlist$`0.9`
+tGLMdf<-data.frame(lambda=tGLM$lambda,metabs=NA,l1Norm=NA)
+for(i in 1:nrow(tGLMdf)){
+  tGLMcoef<-coef(tGLM,s=tGLMdf$lambda[i])
+  tGLMcoef<-cbind(as.matrix(tGLMcoef$sCAD),as.matrix(tGLMcoef$Type.1.MI),
+                  as.matrix(tGLMcoef$Type.2.MI))
+  colnames(tGLMcoef)<-c("sCAD","Type.1.MI","Type.2.MI")
+  if(!is.null(nrow(tGLMcoef[apply(tGLMcoef,1,FUN=function(x) sum(as.integer(x==0))<3),]))){
+    tGLMdf$metabs[i]<-nrow(tGLMcoef[apply(tGLMcoef,1,FUN=function(x) sum(as.integer(x==0))<3),])-1
+    tGLMdf$l1Norm[i]<-sum(abs(tGLMcoef))
+  }
+}
+
+# Plot of L1 norm vs features selected
+tGLMdfLong<-tGLMdf %>% gather(key="Measure",value="value",-lambda)
+p2<-ggplot(tGLMdfLong %>% filter(!is.na(value)),aes(x=lambda,y=value,color=Measure))+geom_line()+theme_bw()+
+  xlab(expression(lambda))+ylab("Value")+
+  scale_color_manual(values=c("darkred","navyblue"),
+    labels=c("L1 Norm","Features (metabolites)\nselected"))
+
+GLMerr<-data.frame(lambda=tGLMcv$lambda,err=log2(exp(tGLMcv$cvm))/2)
+tGLMdf<-tGLMdf %>% full_join(GLMerr)
+p3<-ggplot(tGLMdf ,aes(x=metabs,y=err))+geom_line()+theme_bw()+
+  xlab("Features (metabolites) selected")+ylab("Cross-entropy error (Repeated 10-fold CV)")
+p4<-ggplot(tGLMdf,aes(x=lambda,y=err))+geom_line()+theme_bw()+
+  xlab(expression(lambda))+ylab("Cross-entropy error (Repeated 10-fold CV)")
+
+grid.arrange(p1,p2,p3,p4, ncol = 2, nrow = 2)
+# Check
+tGLMcv<-tGLMcvlist$`0.9`
+set.seed(3)
+cvF1<-cvFolds(n=nrow(metab),K=10,R=10)
+elasticp<-foreach(j=1:cvF1$R, .combine="rbind",.packages="glmnet") %dopar% {
+  elasticp<-data.frame()
+  # K-folds:
+  for(k in 1:cvF1$K){
+    cvF1Df<-data.frame(ind=cvF1$subsets[,j],fold=cvF1$which)
+    elastic1<-glmnet(x=as.matrix(metab[cvF1Df$ind[cvF1Df$fold!=k],names(metab)!="group"]),
+                y=metab[cvF1Df$ind[cvF1Df$fold!=k],"group"],alpha=.9,
+                family="multinomial",lambda=.2)
+    elastic1p<-predict(elastic1,newx=as.matrix(metab[cvF1Df$ind[cvF1Df$fold==k],names(metab)!="group"]),
+                  type="response")
+    elastic1p<-as.data.frame(elastic1p)
+    elastic1p$ptid<-rownames(elastic1p)
+    elasticp<-rbind(elasticp,elastic1p)
+  }
+  elasticp
+}
+elasticp$group<-metab$group[match(elasticp$ptid,rownames(metab))]
+elasticp$ptid<-NULL
+colnames(elasticp)<-gsub(".s0","",colnames(elasticp))
+elasticp$loss<--log(
+  as.numeric(as.matrix(elasticp)[1:nrow(elasticp) + nrow(elasticp) * (match(elasticp$group,colnames(elasticp)) - 1)]))
+mean(elasticp$loss)
